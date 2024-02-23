30b0c8aa01913f68d6b6f221e84d75b3ff5e3231
Piyawut <62786101+WEQAZ@users.noreply.github.com>
2024-02-23 16:56:36 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.welcome')
+@section('title')
+    First Page
+@endsection
+@section('content')
+    <div class="text-center" style="margin-top:2rem">
+        <h1>Welcome to the frist page !!!</h1>
+        <a class="btn btn-primary btn-lg" style="margin-top:1rem" href="/register">Get Started</a>
+    </div>
+@endsection
