5f77eb243e38fbb55e141d3795adfb1ee8bcfdc1
Khim_thanchanok <thanchanok_kaweekul@cmu.ac.th>
2024-03-08 15:36:25 +0700

    add Customer seeder and Category data

--- database/migrations/2024_03_08_040216_create_customers_table.php
+++ database/migrations/2024_03_08_040216_create_customers_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();;
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('password');
             $table->string('email')->unique();
             $table->string('gender');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,29 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        \App\Models\Customer::factory(10)->create();
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $faker = Faker::create();
+        foreach (range(1,50) as $index) {
+            DB :: table('customers') -> insert([
+                'username' => $faker->username,
+                'firstname' => $faker->firstname,
+                'lastname' => $faker->lastname,
+                'password'=> $faker->password,
+                'email'=> $faker->email,
+                'gender' => $faker->randomElement(['male', 'female']),
+                'age' => random_int(18,50),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
