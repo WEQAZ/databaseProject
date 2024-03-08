f2f4e6356f2871405dd8f5e2e5443c48c19759cd
Piyawut <62786101+WEQAZ@users.noreply.github.com>
2024-03-08 13:33:05 +0700

    seed commit

---
+++ database/migrations/2024_03_08_061707_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->string('payment_method');
+            $table->decimal('amount', 8, 2);
+            $table->string('transaction_id')->unique();
+            $table->unsignedBigInteger('status_id');
+            $table->foreign('status_id')->references('id')->on('statuses'); // e.g., 'pending', 'approved', 'failed'
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
