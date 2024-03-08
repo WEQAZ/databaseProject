04c72ae84417f94564ce85a68132fcaf62e07d57
Piyawut <62786101+WEQAZ@users.noreply.github.com>
2024-03-08 11:39:37 +0700

    migrate test 1

---
+++ database/migrations/2024_03_08_042047_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('quantity')->unsigned();
+            $table->decimal('price', 10, 2);
+            $table->bigInteger('category_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
