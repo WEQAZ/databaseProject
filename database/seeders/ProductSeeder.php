0887b8fe38e702dc60930c589b495e5bd6a7b905
Khim_thanchanok <thanchanok_kaweekul@cmu.ac.th>
2024-03-08 16:52:11 +0700

    update

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,50) as $index) {
+            DB :: table('products') -> insert([
+                'username' => $faker->username,
+                'firstname' => $faker->firstname,
+                'lastname' => $faker->lastname,
+                'password'=> $faker->password,
+                'email'=> $faker->email,
+                'gender' => $faker->randomElement(['male', 'female']),
+                'age' => random_int(18,50),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
